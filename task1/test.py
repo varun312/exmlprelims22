e846d0ddb280a123d9d53b8acf40defcb7b7b301
varun312 <32396077+varun312@users.noreply.github.com>
2022-11-08 21:02:18 +0530

    Update test.py

--- task1/test.py
+++ task1/test.py
@@ -1,20 +1,21 @@
-from tensorflow import keras
-model = keras.models.load_model('kerasmodel')
-model.load_weights('first_try.h5')
+import tensorflow as tf
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(2, 2, 3)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(1),
+  tf.keras.layers.Activation('sigmoid')
+])
+
+model.load_weights('pls.h5')
 
 from PIL import Image
 import numpy as np
 
 def predict(path):
     img = Image.open(path)
-    img = img.resize((2800, 2800))
-    left = 1300
-    top = 1300
-    right = 1500
-    bottom = 1500
-    img = img.crop((left, top, right, bottom))
-    img = img.resize((2, 2))
-    
+    img = img.resize((256, 256))
     img = np.array(img)
     img = img /255.0
     img = img.reshape(1, 256, 256, 3)
